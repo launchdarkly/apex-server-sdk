5b3b740ad8bd52af5c91403d39ced37fe2a301cc
Matthew Keeler <mkeeler@launchdarkly.com>
2025-04-02 11:11:27 -0400

    chore: Migrate to release-please

--- .ldrelease/update-version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-TARGET_FILE=bridge/main.go
-TEMP_FILE=${TARGET_FILE}.tmp
-
-sed "s/^	SDK_VERSION .*/	SDK_VERSION   = \"${LD_RELEASE_VERSION}\"/" "${TARGET_FILE}" > "${TEMP_FILE}"
-mv "${TEMP_FILE}" "${TARGET_FILE}"
